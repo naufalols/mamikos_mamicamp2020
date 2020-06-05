909092fe360e41881516bff02f0cdce564e5286a
naufalol <naufalol@hotmail.com>
2020-06-05 19:53:10 +0700

    Episode 9 Go Go Gadget Tilwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="flex item-center mb-4">
+    <div class="container mx-auto">
+        <div class="flex item-center mb-3">
             <h1 class="header h1 mr-auto">Birdboard</h1>
             <a type="button" class="button is-small is-info" href="{{url('/projects/create')}}">New Project</a>
         </div>
-    <ul>
-        @forelse($projects as $project)
-            <li>
-            <a href="{{$project->path()}}"> {{$project->title}}</a></li>
 
+        <div class="flex ">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 p-5 rounded shadow  w-1/3" style="height: 208px">
+                <h3 class="font-normal text-xl mb-4 py-4">{{$project->title}}</h3>
+                <div class="text-gray-600">
+                    {{ str_limit($project->description, 250)}}
+                </div>
+            </div>
         @empty
-            <li>No Projects Yet.</li>
-            
+            <li>No Projects Yet.</li>   
         @endforelse
-    </ul>
+        </div>
     </div>
 @endsection
